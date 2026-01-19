19ec840e2f411ab721e426126de2a4d3cefcb964
Neven <nevenjosipovic5@gmail.com>
2026-01-19 10:56:31 +0100

    Email sender update

---
+++ email_sender/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,jinja,jinja2}",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
