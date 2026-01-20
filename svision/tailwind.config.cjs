890643c1f4202c4fbcc727609abf288163bdbd81
Neven <nevenjosipovic5@gmail.com>
2026-01-20 12:49:03 +0100

    Added Prisila 7 to index

---
+++ svision/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.php",
+    "./admin/**/*.html",
+    "./admin/**/*.php",
+    "./**/*.php",
+    "./**/*.js",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
